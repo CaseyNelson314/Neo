8bafa026ce3aba7a783ada5d37bd8abd721537d7
Casey_Nelson <91818705+CaseyNelson314@users.noreply.github.com>
2024-05-10 20:42:12 +0900

    update

--- sketch_may10a/sketch_may10a.ino
+++ sketch_may10a/sketch_may10a.ino
@@ -1,15 +1,15 @@
 #include <Adafruit_NeoPixel.h>
 
-static Adafruit_NeoPixel pixels(10/*lednum*/, 11/*pin*/, NEO_GRB + NEO_KHZ800);
+static Adafruit_NeoPixel pixels(56/*lednum*/, 11/*pin*/, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   pixels.begin();
 }
 
 void loop() {
-    for (int i = 0; i < 10; ++i)
+    for (int i = 0; i < 56; ++i)
     {
-        pixels.setPixelColor(i,0xffffff);
+        pixels.setPixelColor(i,0xffdddd);
     }
     pixels.show();
     delay(10);
